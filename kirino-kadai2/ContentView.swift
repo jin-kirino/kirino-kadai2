afae845d5cda4b73cdc59150eb116922f97fc8c4
jin-kirino <jinkirino@gmail.com>
2023-02-18 08:50:44 +0900

    レイアウトを作成

--- kirino-kadai2/ContentView.swift
+++ kirino-kadai2/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 割られる数を管理
+    @State private var firstNumber: String = ""
+    // 割る数を管理
+    @State private var secondNumber: String = ""
+    // 計算結果を管理
+    @State private var calculationResults: String = "0.0"
+    // OperatorModelのインスタンス
+    @State private var operatorModel: OperatorModel = .addition
+    // 仮の4つ
+    private let shisokuenzan = ["+", "-", "×", "÷"]
+    @State var selection = "+"
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("", text: $firstNumber)
+            TextField("", text: $secondNumber)
+            Picker(selection: $operatorModel, label: Text("")) {
+                ForEach(Array(OperatorModel.allCases), id: \.self) { index in
+                    Text(index.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
+            Button {
+                // Pickerで選択した演算子で計算する
+                calculationResults = operatorModel.didButtonAction(leftSideNumber: firstNumber, rightSideNumber: secondNumber)
+            } label: {
+                Text("Button")
+            }
+            Text(calculationResults)
         }
-        .padding()
+        .frame(width: 200)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
     }
 }
 
